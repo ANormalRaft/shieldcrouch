25fb03867c5a5343ec26c9b8ff2ddf3e52905434
ANormalRaft <bastian5417@hotmail.com>
2026-09-01 19:50:29 -0400

    Mod now includes small optional misc additions which don't fit in my other mods, starting with making poison lethal

--- src/main/java/com/anormalraft/shieldcrouch/Config.java
+++ src/main/java/com/anormalraft/shieldcrouch/Config.java
@@ -8,8 +8,12 @@ public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     public static final ModConfigSpec.BooleanValue SHIELD_CROUCH = BUILDER
-            .comment("Enabled or disables the mod: should the shield be only activated on crouch instead of right click. Will disable right click for the shield \nDefault: true")
+            .comment("Should the shield be only activated on crouch instead of right click. Will disable right click for the shield if true \nDefault: true")
             .define("shieldCrouch", true);
+
+    public static final ModConfigSpec.BooleanValue LETHAL_POISON = BUILDER
+            .comment("Should vanilla poison be lethal \nDefault: false")
+            .define("lethalPoison", false);
 
     static final ModConfigSpec SPEC = BUILDER.build();
 }
---
+++ src/main/java/com/anormalraft/shieldcrouch/mixin/PoisonMobEffectMixin.java
@@ -0,0 +1,29 @@
+package com.anormalraft.shieldcrouch.mixin;
+
+import com.anormalraft.shieldcrouch.Config;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+//Thank you Fabric mixin docs https://wiki.fabricmc.net/tutorial:mixin_tips
+@Mixin(targets = "net.minecraft.world.effect.PoisonMobEffect")
+public class PoisonMobEffectMixin extends MobEffect {
+
+    protected PoisonMobEffectMixin(MobEffectCategory category, int color) {
+        super(category, color);
+    }
+
+    //Makes the 1 hp health check always true by giving it a 2 regardless of player health
+    @Redirect(method = "applyEffectTick", at = @At(value="INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getHealth()F"))
+    public float removeOneHpStop(LivingEntity instance){
+        if(Config.LETHAL_POISON.getAsBoolean()) {
+            return 2.0F;
+        } else {
+            return instance.getHealth();
+        }
+    }
+
+}
